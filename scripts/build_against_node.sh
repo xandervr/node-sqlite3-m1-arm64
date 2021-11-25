919e3a3fc17b7cfdafdaa8e83029c6445567c321
Xander Van Raemdonck <xandervanraemdonck@gmail.com>
2021-11-25 11:14:21 +0100

    INIT

--- scripts/build_against_node.sh
+++ scripts/build_against_node.sh
@@ -4,6 +4,8 @@ source ~/.nvm/nvm.sh
 
 set -e -u
 
+# -I/opt/homebrew/Cellar/zeromq/4.3.4/include
+
 function publish() {
     if [[ ${PUBLISHABLE:-false} == true ]] && [[ ${COMMIT_MESSAGE} =~ "[publish binary]" ]]; then
         CFLAGS="${CFLAGS:-} -include $(pwd)/src/gcc-preinclude.h" CXXFLAGS="${CXXFLAGS:-} -include $(pwd)/src/gcc-preinclude.h" node-pre-gyp rebuild  --clang=1
